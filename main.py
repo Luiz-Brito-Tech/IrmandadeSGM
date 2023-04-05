697e7bbc40fea802ce0da3dc256e9bb1f1073300
Luiz Brito <luizbritotech@gmail.com>
2023-04-05 11:50:02 -0300

    05/04/23 - 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__)
 app_context = app.app_context()
@@ -12,7 +12,15 @@ def login():
     if request.method == 'GET':
         return render_template("login.html")
     elif request.method == 'POST':
-        return redirect(url_for("homepage"))
+        username = request.form['username']
+        password = request.form['password']
+        session['user', 'password'] = username, password
+        return redirect(url_for("painel", username=username, password=password))
+
+@app.route('/painel')
+def dashboard(username, password):
+    return render_template("dashboard.html")
+
 
 @app.route('/artigos')
 def articles():
