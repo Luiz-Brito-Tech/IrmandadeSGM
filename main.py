dc945525df72f4fe9a173855836af14fccf29c66
Luiz Brito <luizbritotech@gmail.com>
2023-04-05 12:27:10 -0300

    05/04/23 - 3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__)
 app_context = app.app_context()
+app.secret_key = 'relou'
 
 @app.route('/')
 def homepage():
@@ -12,19 +13,19 @@ def login():
     if request.method == 'GET':
         return render_template("login.html")
     elif request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        session['user', 'password'] = username, password
-        return redirect(url_for("painel", username=username, password=password))
+        for key in request.form.keys():
+            session[key] = request.form[key]
+        return redirect(url_for("dashboard"))
 
 @app.route('/painel')
-def dashboard(username, password):
-    return render_template("dashboard.html")
-
+def dashboard():
+    if 'username' in session:
+        username = session['username']
+        return render_template("dashboard.html", username=username)
 
 @app.route('/artigos')
 def articles():
     return render_template("articles.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0')
